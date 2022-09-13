50c3464b7ae2a95a30af2f7df707fe80035cedbb
Rober <boblawyer1@gmail.com>
2022-09-13 11:08:05 -0300

    terminada la seccion 'dinamicos'

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,14 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+interface Persona {
+  nombre: string;
+  favoritos: Favorito[];
+}
+
+interface Favorito {
+  id: number;
+  nombre: string;
+}
 
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html'
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent  {
 
-  constructor() { }
 
-  ngOnInit(): void {
+  guardar() {
+    console.log("formulario posteado")
   }
 
+  agregarJuego() {
+    const nuevoFavorito: Favorito = {
+      id: this.persona.favoritos.length + 1,
+      nombre: this.nuevoJuego
+    }
+
+    this.persona.favoritos.push({...nuevoFavorito});
+    this.nuevoJuego='';
+  }
+
+  eliminar(index: number) {
+    this.persona.favoritos.splice(index, 1)
+  }
+
+nuevoJuego: string = '';
+persona : Persona = {
+  nombre: "roberto",
+  favoritos: [{id: 1, nombre : "Manic Miner"}, {id: 2, nombre: 'Highway Encounter'}]
+}
 }
